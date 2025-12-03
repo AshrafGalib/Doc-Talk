d9b3dbd0639e5e6fbd798cd82807f8450a977732
AshrafGalib <galibashraf.ag@gmail.com>
2025-12-03 08:57:05 +0600

    added local storage

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -3,42 +3,41 @@ import Navbar from '../../components/Header/Navbar';
 import { Outlet} from 'react-router';
 import Footer from '../../components/Footer/Footer';
 import { BookDocContext } from '../../../public/Context/bookContext';
-import { useState } from 'react';
+import {   useState } from 'react';
 import { toast } from 'react-toastify';
-
-
+import { getBookedDocFromLocalStorage, setBookedDocToLocalStorage } from '../../utilities/localStorage';
 
 const Root = () => {
-    const [bookedDoctors,setBookedDoctors]=useState([])
-    const handleBook =(doctor)=>{
-        const checkDoctor = bookedDoctors.some(d=>d.id===doctor.id)
-        console.log(checkDoctor)
-        if(checkDoctor){
-               toast('Appointment already scheduled for today',{type:'error'})
-               //return bookedDoctors
-        }
-        else{
-            toast('Booked',{type:'success'})
-            const newBookedDoctors =[...bookedDoctors,doctor]
-        setBookedDoctors(newBookedDoctors)
-        }
-    }
-    const handleCancelBook=(id)=>{
-       const newBookedDoctorsAfterCancelation = bookedDoctors.filter(doc=>
-       id!==doc.id)
-        setBookedDoctors(newBookedDoctorsAfterCancelation)
-    }
+    const [bookedDoctors, setBookedDoctors] = useState(() => {
+        return getBookedDocFromLocalStorage();
+    });
+    const handleBook = (doctor) => {
+        const alreadyBooked = bookedDoctors.some(d => d.id === doctor.id);
 
+        if (alreadyBooked) {
+            toast("Appointment already scheduled for today", { type: "error" });
+            return;
+        }
 
-    return (
+        toast(`Appointment scheduled for ${doctor.name} successfully`, { type: "success" });
+
+        const updated = [...bookedDoctors, doctor];
+        setBookedDoctors(updated);
+        setBookedDocToLocalStorage(doctor);
+    };
+
+    const handleCancelBook = (id) => {
+        const updated = bookedDoctors.filter(doc => doc.id !== id);
+        setBookedDoctors(updated);
+        localStorage.setItem("Booked Doctor", JSON.stringify(updated));
+    }
+ return (
         <div className= 'w-11/12 mx-auto'>
             
             <Navbar></Navbar>
            <BookDocContext.Provider value ={{bookedDoctors,handleBook,handleCancelBook}} >
                <Outlet></Outlet>
            </BookDocContext.Provider>
-                
-           
             <Footer></Footer>
 
         </div>
